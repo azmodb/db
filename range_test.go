a32b544e17e6d591a4c0e309dc377e090962da4a
Markus Sonderegger <marraison@gmail.com>
2016-09-12 19:37:54 +0200

    initial range unit tests

--- range_test.go
+++ range_test.go
@@ -1,30 +1,46 @@
 package db
 
-/*
-func TestDBGet(t *testing.T) {
-	db := newDB(nil)
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRange(t *testing.T) {
+	db := New()
 	b := db.Next()
 	b.Insert([]byte("k1"), []byte("v1.1"), false)
-	b.Insert([]byte("k2"), []byte("v2.1"), false)
-	b.Insert([]byte("k3"), []byte("v3.1"), false)
-	b.Insert([]byte("k4"), []byte("v4.1"), false)
 	b.Insert([]byte("k1"), []byte("v1.2"), false)
 	b.Insert([]byte("k1"), []byte("v1.3"), false)
+
+	b.Put([]byte("k2"), []byte("v2.1"), false)
+	b.Put([]byte("k2"), []byte("v2.2"), false)
+
+	b.Increment([]byte("n1"), 1, false)
+	b.Increment([]byte("n1"), 1, false)
+	b.Increment([]byte("n1"), 1, false)
+
+	b.Increment([]byte("n2"), 1, false)
+	b.Decrement([]byte("n2"), 1, false)
+	b.Decrement([]byte("n2"), 1, false)
 	b.Commit()
 
-	rec, _ := db.Get([]byte("k1"), 0, true)
-	fmt.Println(rec)
-	rec, _ = db.Get([]byte("k1"), 0, false)
-	fmt.Println(rec)
-
-	rec, _ = db.Get([]byte("k1"), 4, true)
-	fmt.Println(rec)
-	rec, _ = db.Get([]byte("k1"), 4, false)
-	fmt.Println(rec)
-
-	db.Range([]byte("k1"), []byte("k5"), 0, false, func(k []byte, r *Record) bool {
-		fmt.Println(string(k), r)
+	testFunc := func(key []byte, rec *Record) bool {
+		fmt.Println(string(key), rec)
 		return false
-	})
+	}
+
+	tests := []struct {
+		from, to []byte
+		rev      int64
+		vers     bool
+		fn       RangeFunc
+	}{
+		{nil, nil, 0, true, testFunc},
+		{nil, []byte("n2"), 0, true, testFunc},
+		{[]byte("k2"), nil, 0, true, testFunc},
+	}
+	for _, test := range tests {
+		db.Range(test.from, test.to, test.rev, test.vers, test.fn)
+		fmt.Println("----")
+	}
 }
-*/
