45489618d32429cbb109c5b74f7b767ef04ef7ea
Markus Sonderegger <marraison@gmail.com>
2016-09-12 09:29:33 +0200

    import initial backend db

--- backend/backend.go
+++ backend/backend.go
@@ -1,10 +1,8 @@
 package backend
 
-type RangeFunc func(key, value []byte) (done bool)
-
 type Backend interface {
+	Range(fn func(key, value []byte) bool) error
 	Next(rev int64) (Batch, error)
-	Range(fn RangeFunc) error
 }
 
 type Batch interface {
