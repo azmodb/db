3d9424a2834fd9ded704938d15837e34a7ea9962
Markus Sonderegger <marraison@gmail.com>
2016-09-13 11:02:27 +0200

    improve initial range unit test

--- range_test.go
+++ range_test.go
@@ -1,46 +1,69 @@
 package db
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/azmodb/db/pb"
 )
 
-func TestRange(t *testing.T) {
-	db := New()
-	b := db.Next()
-	b.Insert([]byte("k1"), []byte("v1.1"), false)
-	b.Insert([]byte("k1"), []byte("v1.2"), false)
-	b.Insert([]byte("k1"), []byte("v1.3"), false)
+var (
+	wantRec1 = &Record{
+		Record: &pb.Record{Type: unicode, Current: 11, Values: []*pb.Value{
+			&pb.Value{Unicode: []byte("v1.1"), Rev: 1},
+			&pb.Value{Unicode: []byte("v1.2"), Rev: 2},
+			&pb.Value{Unicode: []byte("v1.3"), Rev: 3},
+		}},
+	}
+)
 
-	b.Put([]byte("k2"), []byte("v2.1"), false)
-	b.Put([]byte("k2"), []byte("v2.2"), false)
-
-	b.Increment([]byte("n1"), 1, false)
-	b.Increment([]byte("n1"), 1, false)
-	b.Increment([]byte("n1"), 1, false)
-
-	b.Increment([]byte("n2"), 1, false)
-	b.Decrement([]byte("n2"), 1, false)
-	b.Decrement([]byte("n2"), 1, false)
-	b.Commit()
-
-	testFunc := func(key []byte, rec *Record) bool {
-		fmt.Println(string(key), rec)
-		return false
+func testDefaultRange(t *testing.T, db *DB) {
+	testFunc := func(i int, values []*pb.Value) RangeFunc {
+		return func(key []byte, rec *Record) bool {
+			if !reflect.DeepEqual(values, rec.Values) {
+				t.Fatalf("range #%d: values mismatch\n%+v\n%+v", i, values, rec.Values)
+			}
+			rec.Close()
+			return false
+		}
 	}
 
 	tests := []struct {
 		from, to []byte
 		rev      int64
 		vers     bool
-		fn       RangeFunc
+		values   []*pb.Value
+		fn       func(i int, values []*pb.Value) RangeFunc
 	}{
-		{nil, nil, 0, true, testFunc},
-		{nil, []byte("n2"), 0, true, testFunc},
-		{[]byte("k2"), nil, 0, true, testFunc},
+		{nil, nil, -1, false, wantRec1.Values[len(wantRec1.Values)-1:], testFunc},
+		{nil, nil, -1, true, wantRec1.Values, testFunc},
+		{nil, nil, 0, false, wantRec1.Values[len(wantRec1.Values)-1:], testFunc},
+		{nil, nil, 0, true, wantRec1.Values, testFunc},
+
+		{nil, nil, 1, false, wantRec1.Values[0:1], testFunc},
+		{nil, nil, 2, false, wantRec1.Values[1:2], testFunc},
+		{nil, nil, 3, false, wantRec1.Values[2:3], testFunc},
+		{nil, nil, 1, true, wantRec1.Values[0:], testFunc},
+		{nil, nil, 2, true, wantRec1.Values[1:], testFunc},
+		{nil, nil, 3, true, wantRec1.Values[2:], testFunc},
+
+		{nil, nil, 4, false, nil, testFunc},
+		{nil, nil, 4, true, nil, testFunc},
+		{nil, nil, 5, false, nil, testFunc},
+		{nil, nil, 5, true, nil, testFunc},
 	}
-	for _, test := range tests {
-		db.Range(test.from, test.to, test.rev, test.vers, test.fn)
-		fmt.Println("----")
+	for i, test := range tests {
+		db.Range(test.from, test.to, test.rev, test.vers, test.fn(i, test.values))
 	}
+}
+
+func TestRevisionRange(t *testing.T) {
+	db := New()
+	b := db.Next()
+	b.Insert([]byte("k1"), []byte("v1.1"), false)
+	b.Insert([]byte("k1"), []byte("v1.2"), false)
+	b.Insert([]byte("k1"), []byte("v1.3"), false)
+	b.Commit()
+
+	testDefaultRange(t, db)
 }
