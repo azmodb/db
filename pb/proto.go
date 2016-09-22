881064887ba07778ac70affd136a9d547960175c
Markus Sonderegger <marraison@gmail.com>
2016-09-22 12:10:06 +0200

    add protobuf helper

--- pb/proto.go
+++ pb/proto.go
@@ -2,7 +2,17 @@
 
 package pb
 
+import "github.com/gogo/protobuf/proto"
+
 const (
 	Numeric = Type_Numeric
 	Unicode = Type_Unicode
 )
+
+func MustMarshal(m proto.Message) []byte {
+	data, err := proto.Marshal(m)
+	if err != nil {
+		panic("protobuf marshal failed: " + err.Error())
+	}
+	return data
+}
